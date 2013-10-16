abc5d96364d3a5fcdd7c094a933ed4b9c26ee145
Maxime Haineault <max@motion-m.ca>
2013-10-16 10:51:09 -0400

    Updated urls

--- project_name/urls.py
+++ project_name/urls.py
@@ -7,18 +7,17 @@ from django.contrib import admin
 #from dajaxice.core import dajaxice_autodiscover
 
 admin.autodiscover()
-dajaxice_autodiscover()
+#dajaxice_autodiscover()
 
-js_info_dict = {
-    'packages': ('django.conf',),
-}
+#js_info_dict = {'packages': ('django.conf',),}
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
     (r'^i18n/',      include('django.conf.urls.i18n')),
    #(r'^jsi18n$',       'django.views.i18n.javascript_catalog', js_info_dict),
-    (r'^grappelli/', include('grappelli.urls')),
+   #(r'^grappelli/', include('grappelli.urls')),
+   #(r'^dajaxice/', include('dajaxice.urls')),
    #(r'^favicon.ico$', 'django.views.generic.simple.redirect_to', {
    #    'url': '%swebsite/img/favicon.ico' % settings.STATIC_URL}),
 )
