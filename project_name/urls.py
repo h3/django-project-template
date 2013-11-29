60c59b46c9f9e8ce4816a1648bb064c4fb8d2737
Maxime Haineault <max@motion-m.ca>
2013-11-28 21:45:42 -0500

    Update urls.py

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls.defaults import *
+from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 #from dajaxice.core import dajaxice_autodiscover
+#dajaxice_autodiscover()
 
 admin.autodiscover()
-#dajaxice_autodiscover()
 
 #js_info_dict = {'packages': ('django.conf',),}
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
+    (r'^i18n/', include('django.conf.urls.i18n')),
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    (r'^i18n/',      include('django.conf.urls.i18n')),
-   #(r'^jsi18n$',       'django.views.i18n.javascript_catalog', js_info_dict),
+   #(r'^jsi18n$', 'django.views.i18n.javascript_catalog', js_info_dict),
    #(r'^grappelli/', include('grappelli.urls')),
    #(r'^dajaxice/', include('dajaxice.urls')),
-   #(r'^favicon.ico$', 'django.views.generic.simple.redirect_to', {
-   #    'url': '%swebsite/img/favicon.ico' % settings.STATIC_URL}),
+   #url(r'^favicon.ico$', RedirectView.as_view(
+   #url='%swebsite/img/favicon.ico' % settings.STATIC_URL)),
 )
 
 urlpatterns += staticfiles_urlpatterns()
